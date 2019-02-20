19c6cd31e62ea3f59cfb3567e3504fc492fa82f6
David.Gao <david.alpha.fox@gmail.com>
2019-02-20 16:16:35 +0800

    fix bug: when render ai_dom_node the id of the element does not render in atrributes

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 PROJECT = aihtml
 PROJECT_DESCRIPTION = html tool for productions from ailink.io
-PROJECT_VERSION = 0.2.1
+PROJECT_VERSION = 0.2.2
 
 ERLC_OPTS = -Werror +debug_info +warn_export_vars +warn_shadow_vars +warn_obsolete_guard
 DEPS = ailib
--- src/ai_dom_render.erl
+++ src/ai_dom_render.erl
@@ -5,69 +5,76 @@ render(Node)->
 		render([Node],[],<<>>).
 render(undefined,Node)-> ai_string:to_string(ai_dom_node:value(Node));
 render(meta,Node)->
-		Attributes = attributes(ai_dom_node:attributes(Node)),
+		Attributes = attributes(Node),
 		<<"<meta",Attributes/binary,"/>">>;
 render(area,Node)->
-		Attributes = attributes(ai_dom_node:attributes(Node)),
+		Attributes = attributes(Node),
 		<<"<area",Attributes/binary,"/>">>;
 render(base,Node)->
-		Attributes = attributes(ai_dom_node:attributes(Node)),
+		Attributes = attributes(Node),
 		<<"<base",Attributes/binary,"/>">>;
 render(br,Node)->
-		Attributes = attributes(ai_dom_node:attributes(Node)),
+		Attributes = attributes(Node),
 		<<"<br",Attributes/binary,"/>">>;
 render(col,Node)->
-		Attributes = attributes(ai_dom_node:attributes(Node)),
+		Attributes = attributes(Node),
 		<<"<col",Attributes/binary,"/>">>;
 render(embed,Node)->
-		Attributes = attributes(ai_dom_node:attributes(Node)),
+		Attributes = attributes(Node),
 		<<"<embed",Attributes/binary,"/>">>;
 render(hr,Node)->
-		Attributes = attributes(ai_dom_node:attributes(Node)),
+		Attributes = attributes(Node),
 		<<"<hr",Attributes/binary,"/>">>;
 render(img,Node)->
-		Attributes = attributes(ai_dom_node:attributes(Node)),
+		Attributes = attributes(Node),
 		<<"<img",Attributes/binary,"/>">>;
 render(input,Node)->
-		Attributes = attributes(ai_dom_node:attributes(Node)),
+		Attributes = attributes(Node),
 		<<"<input",Attributes/binary,"/>">>;
 render(link,Node)->
-		Attributes = attributes(ai_dom_node:attributes(Node)),
+		Attributes = attributes(Node),
 		<<"<link",Attributes/binary,"/>">>;
 render(param,Node)->
-		Attributes = attributes(ai_dom_node:attributes(Node)),
+		Attributes = attributes(Node),
 		<<"<param",Attributes/binary,"/>">>;
 render(source,Node)->
-		Attributes = attributes(ai_dom_node:attributes(Node)),
+		Attributes = attributes(Node),
 		<<"<source",Attributes/binary,"/>">>;
 render(track,Node)->
-		Attributes = attributes(ai_dom_node:attributes(Node)),
+		Attributes = attributes(Node),
 		<<"<track",Attributes/binary,"/>">>;
 render(wbr,Node)->
-		Attributes = attributes(ai_dom_node:attributes(Node)),
+		Attributes = attributes(Node),
 		<<"<wbr",Attributes/binary,"/>">>;
 render(script,Node)->
-		Attributes = attributes(ai_dom_node:attributes(Node)),
+		Attributes = attributes(Node),
 		<<"<script",Attributes/binary,"/>">>;
 render(Tag,Node) ->
 		TagBin = ai_string:to_string(Tag),
 		Value = ai_string:to_string(ai_dom_node:value(Node)),
-		Attributes = attributes(ai_dom_node:attributes(Node)),
+		Attributes = attributes(Node),
 		<<"<",TagBin/binary,Attributes/binary,">",Value/binary,"</",TagBin/binary,">">>.
 render([],[],Acc)-> Acc;
 render([],[{Parent,Rest,OldAcc}|Stack],Acc)->
 		TagBin = ai_string:to_string(ai_dom_node:tag(Parent)),
-		Attributes = attributes(ai_dom_node:attributes(Parent)),
+		Attributes = attributes(Parent),
 		Acc1 = <<"<",TagBin/binary,Attributes/binary,">",Acc/binary,"</",TagBin/binary,">">>,
 		render(Rest,Stack,<<OldAcc/binary,Acc1/binary>>);
-render([El|Rest],Stack,Acc) -> 
-	case ai_dom_node:children(El) of 
+render([El|Rest],Stack,Acc) ->
+	case ai_dom_node:children(El) of
 		[]->
 				NodeBin = render(ai_dom_node:tag(El),El),
 				render(Rest,Stack,<<Acc/binary,NodeBin/binary>>);
 		Children -> render(Children,[{El,Rest,Acc}|Stack],<<>>)
 	end.
-attributes(Attributes)->	
+
+attributes(Node)->
+    Attributes = ai_dom_node:attributes(Node),
+    Attributes0 =
+        case ai_dom_node:id(Node) of
+            undefined -> Attributes;
+            ID -> maps:put(id,ID,Attributes)
+        end,
 		lists:foldl(fun({K,V},Acc)->
 									Attr = ai_string:to_string(K),
 									case V of
@@ -76,6 +83,5 @@ attributes(Attributes)->
 											AttrValue = ai_string:to_string(V),
 											<<Acc/binary,Attr/binary,"=\"",AttrValue/binary,"\" ">>
 									end
-								end,<<" ">>,maps:to_list(Attributes)).
+								end,<<" ">>,maps:to_list(Attributes0)).
 
-		
