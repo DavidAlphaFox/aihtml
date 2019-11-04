e791dd63f1aaab228978d8a0ee60671dc6415e84
David.Gao <david.alpha.fox@gmail.com>
2019-11-04 18:12:40 +0800

    token

--- src/ai_markdown_parser.erl
+++ src/ai_markdown_parser.erl
@@ -9,12 +9,12 @@ parse(Markdown) ->
 tokenize(<<>>,<<>>,Acc)-> lists:reverse(Acc);
 tokenize(<<>>,Buffer, Acc)-> lists:reverse(maybe_buffer(Buffer,Acc));
 
-tokenize(<<"-" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest,<<>>, [{{md, dash}, "-"} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"#" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{{md, atx}, "#"} |  maybe_buffer(Buffer,Acc)]);
-tokenize(<<">" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{{md, gt}, ">"} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"+" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{{md, plus}, "+"} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"*" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{{md, star}, "*"} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"_" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{{md, underscore}, "_"} |  maybe_buffer(Buffer,Acc)]);
+tokenize(<<"-" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest,<<>>, [minus | maybe_buffer(Buffer,Acc)]);
+tokenize(<<"#" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [hash |  maybe_buffer(Buffer,Acc)]);
+tokenize(<<">" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [gt | maybe_buffer(Buffer,Acc)]);
+tokenize(<<"+" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [plus | maybe_buffer(Buffer,Acc)]);
+tokenize(<<"*" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [star | maybe_buffer(Buffer,Acc)]);
+tokenize(<<"_" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [underscore |  maybe_buffer(Buffer,Acc)]);
 tokenize(<<"1" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{num, "1"} | maybe_buffer(Buffer,Acc)]);
 tokenize(<<"2" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{num, "2"} | maybe_buffer(Buffer,Acc)]);
 tokenize(<<"3" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{num, "3"} | maybe_buffer(Buffer,Acc)]);
@@ -25,22 +25,22 @@ tokenize(<<"7" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{num, "7"} 
 tokenize(<<"8" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{num, "8"} | maybe_buffer(Buffer,Acc)]);
 tokenize(<<"9" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{num, "9"} | maybe_buffer(Buffer,Acc)]);
 tokenize(<<"0" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{num, "0"} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"." , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{{punc, fullstop}, "."} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<":" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{{punc, colon}, ":"} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"'" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{{punc, singleq}, "'"} | maybe_buffer(Buffer,Acc)]); %'
-tokenize(<<"\"" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{{punc, doubleq}, "\""} | maybe_buffer(Buffer,Acc)]); %"
-tokenize(<<"`" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{{punc, backtick}, "`"} | maybe_buffer(Buffer,Acc)]); %"
-tokenize(<<"!" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{{punc, bang}, "!"} | maybe_buffer(Buffer,Acc)]); %"
-tokenize(<<"\\" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{{punc, bslash}, "\\"} | maybe_buffer(Buffer,Acc)]); %"
-tokenize(<<"/" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{{punc, fslash}, "/"} | maybe_buffer(Buffer,Acc)]); %"
-tokenize(<<"(" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{bra, "("} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<")" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{ket, ")"} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"[" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{{inline, open}, "["} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"]" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{{inline, close}, "]"} | maybe_buffer(Buffer,Acc)]);
+tokenize(<<"." , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [fullstop | maybe_buffer(Buffer,Acc)]);
+tokenize(<<":" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [colon | maybe_buffer(Buffer,Acc)]);
+tokenize(<<"'" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [single_quote | maybe_buffer(Buffer,Acc)]);
+tokenize(<<"\"" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [double_quote | maybe_buffer(Buffer,Acc)]);
+tokenize(<<"`" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [back_tick | maybe_buffer(Buffer,Acc)]);
+tokenize(<<"!" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [bang | maybe_buffer(Buffer,Acc)]);
+tokenize(<<"\\" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [back_slash | maybe_buffer(Buffer,Acc)]);
+tokenize(<<"/" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [slash | maybe_buffer(Buffer,Acc)]); 
+tokenize(<<"(" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{parentheses, open} | maybe_buffer(Buffer,Acc)]);
+tokenize(<<")" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{parentheses, close} | maybe_buffer(Buffer,Acc)]);
+tokenize(<<"[" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{bracket, open} | maybe_buffer(Buffer,Acc)]);
+tokenize(<<"]" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{bracket, close} | maybe_buffer(Buffer,Acc)]);
 tokenize(<<"\s" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{{ws, sp}, " "} | maybe_buffer(Buffer,Acc)]);
 tokenize(<<"\t" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{{ws, tab}, "\t"} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"\r\n", Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{{lf, crlf}, "\r\n"} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"\n", Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{{lf, lf}, "\n"} | maybe_buffer(Buffer,Acc)]);
+tokenize(<<"\r\n", Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{lf, crlf} | maybe_buffer(Buffer,Acc)]);
+tokenize(<<"\n", Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{lf, lf} | maybe_buffer(Buffer,Acc)]);
 tokenize(<<C/utf8,Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<Buffer/binary,C/utf8>> , Acc).
 
 maybe_buffer(<<>>,Acc) -> Acc;
