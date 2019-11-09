5a9ef9ba7c537c8cf1757a462fb8e7e3ad683605
David.Gao <david.alpha.fox@gmail.com>
2019-11-09 22:16:46 +0800

    [Feature] paragraph and block quote

--- src/ai_markdown_parser.erl
+++ src/ai_markdown_parser.erl
@@ -1,211 +1,65 @@
 -module(ai_markdown_parser).
--export([parse/1]).
 -export([scan_block/1]).
 -record(node,{
               kind,
               attrs = [],
+              children = [],
               value = <<>>
              }).
-
-parse(Markdown) ->
-    Tokens = tokenize(Markdown,<<>>,[]),
-    parse(Tokens,[],[]).
-
-
-tokenize(<<>>,<<>>,Acc)-> lists:reverse(Acc);
-tokenize(<<>>,Buffer, Acc)-> lists:reverse(maybe_buffer(Buffer,Acc));
-
-tokenize(<<"-" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest,<<>>, [minus | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"#" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [hash |  maybe_buffer(Buffer,Acc)]);
-tokenize(<<">" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [gt | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"+" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [plus | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"*" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [star | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"_" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [underscore |  maybe_buffer(Buffer,Acc)]);
-tokenize(<<"1" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{num, "1"} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"2" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{num, "2"} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"3" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{num, "3"} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"4" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{num, "4"} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"5" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{num, "5"} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"6" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{num, "6"} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"7" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{num, "7"} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"8" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{num, "8"} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"9" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{num, "9"} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"0" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{num, "0"} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"." , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [dot | maybe_buffer(Buffer,Acc)]);
-tokenize(<<":" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [colon | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"'" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [single_quote | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"\"" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [double_quote | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"`" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [back_tick | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"!" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [bang | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"\\" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [back_slash | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"/" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [slash | maybe_buffer(Buffer,Acc)]); 
-tokenize(<<"(" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{parentheses, open} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<")" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{parentheses, close} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"[" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{bracket, open} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"]" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{bracket, close} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"\s" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, maybe_combine([{{ws, sp}, " "} | maybe_buffer(Buffer,Acc)]));
-tokenize(<<"\t" , Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, maybe_combine([{{ws, tab}, "\t"} | maybe_buffer(Buffer,Acc)]));
-tokenize(<<"\r\n", Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{lf, crlf} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<"\n", Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<>>, [{lf, lf} | maybe_buffer(Buffer,Acc)]);
-tokenize(<<C/utf8,Rest/binary>>, Buffer, Acc)-> tokenize(Rest, <<Buffer/binary,C/utf8>> , Acc).
-
-maybe_buffer(<<>>,Acc) -> Acc;
-maybe_buffer(Buffer,Acc)-> [{binary,Buffer}|Acc].
-
-
-maybe_combine([{{ws,_},W1},{{ws,_},W2}|Acc]) -> [{{ws,comb},W1 ++ W2}|Acc];
-maybe_combine(Acc) -> Acc.
-
-parse([],[],Acc) -> reduce(Acc);
-parse([],Stack,Acc) ->
-    Lines = reduce(lists:reverse(Stack)),
-    reduce(Lines ++ Acc);
-parse([{lf,_}|T],[],Acc)->
-    parse(T,[],[blank|Acc]);
-parse([{lf,_}|T],Stack,Acc)->
-    Lines = reduce(lists:reverse(Stack)),
-    parse(T,[],Lines ++ Acc);
-parse([{{ws,tab},_} | T ],[],Acc)->
-    {Content,Rest} = binary_line(T,[{binary,<<>>}]),
-    parse(Rest,[],[{pre,Content}|Acc]);
-parse([{{ws,comb},[$\s,$\s,$\s,$\s|W]}|T],[],Acc)->
-    {Content,Rest} = binary_line([{{ws,comb},W} | T],[{binary,<<>>}]),
-    parse(Rest,[],[{pre,Content}|Acc]);
-parse([{{ws,comb},[$\t|W]} | T ],[],Acc)->
-    {Content,Rest} = binary_line([{{ws,comb},W} | T],[{binary,<<>>}]),
-    parse(Rest,[],[{pre,Content}|Acc]);
-parse([gt|T],[],Acc)->
-    {Line,Rest} = strip_line(T,[]),
-    Content = parse(Line,[],[]),
-    parse(Rest,[],[{quote,Content}|Acc]);
-parse([{num,N2}|T],[{num,N1}|ST],Acc)->
-    parse(T,[{num,N1 ++ N2}|ST],Acc);
-parse([dot|T],[{num,_}],[{ol,N,_}|_T] = Acc)->
-    parse(T,[{ol,N+1}],Acc);
-parse([dot|T],[{num,_}],Acc)->
-    parse(T,[{ol,1}],Acc);
-
-parse([minus|T],[],Acc)->
-    parse(T,[li],Acc);
-parse([star|T],[],Acc)->
-    parse(T,[li],Acc);
-
-parse([back_tick|T],[],Acc)->
-    parse(T,[back_tick],Acc);
-parse([back_tick|T],Stack,Acc)->
-    NewStack = reduce(Stack,[],back_tick),
-    parse(T,NewStack,Acc);
-
-parse([underscore|T],[],Acc)->
-    parse(T,[underscore],Acc);
-parse([underscore|T],Stack,Acc)->
-    NewStack = reduce(Stack,[],underscore),
-    parse(T,NewStack,Acc);
-
-parse([hash|T],[],Acc) ->
-    parse(T,[{hash,1}], Acc);
-parse([hash|T],[{hash,N}],Acc) ->
-    parse(T,[{hash,N+1}],Acc);
-
-
-parse([H|T],Stack,Acc) ->
-    parse(T,[H|Stack],Acc).
-
-
-strip_line([],Acc)->{lists:reverse(Acc),[]};
-strip_line([{lf,_}|T],Acc)->{lists:reverse(Acc),T};
-strip_line([H|T],Acc) -> strip_line(T,[H|Acc]).
-
-binary_line([],[Acc])->{Acc,[]};
-binary_line([{lf,_}|T],[Acc])->{Acc,T};
-binary_line([H|T],Acc) ->
-    NewAcc = token_to_string(H,Acc),
-    binary_line(T,NewAcc).
-
-
-reduce([],Stack,Expect)->[Expect|lists:reverse(Stack)];
-reduce([Expect|T],Stack,Expect)-> [{Expect,lists:reverse(reduce(Stack))}|T];
-reduce([H|T],Stack,Expect)-> reduce(T,[H|Stack],Expect).
-
-reduce([li| T])->
-    Stack = lists:foldl(fun token_to_string/2,[],T),
-    [{li,Stack}];
-reduce([{ol,N}|T])->
-    Stack = lists:foldl(fun token_to_string/2,[],T),
-    [{ol,N,Stack}];
-reduce([{hash,N}|T]) ->
-    Stack = lists:foldl(fun token_to_string/2,[],T),
-    [{h,N,Stack}];
-reduce(L)-> lists:foldl(fun token_to_string/2,[],L).
-
-token_to_string({num,N},Acc) ->
-    BinNum = ai_string:to_string(N),
-    maybe_append(Acc,BinNum);
-token_to_string(minus,Acc) -> maybe_append(Acc,<<$->>);
-token_to_string(plus,Acc) -> maybe_append(Acc,<<$+>>);
-token_to_string(star,Acc) -> maybe_append(Acc,<<$*>>);
-token_to_string(gt,Acc) ->  maybe_append(Acc,<< $> >>);
-token_to_string(underscore,Acc)-> maybe_append(Acc,<<$_>>);
-token_to_string(dot,Acc) -> maybe_append(Acc,<<$.>>);
-token_to_string(colon,Acc)-> maybe_append(Acc,<<$:>>);
-token_to_string(single_quote,Acc) -> maybe_append(Acc,<< $'>>);
-token_to_string(double_quote,Acc) -> maybe_append(Acc,<<$">>);
-token_to_string(back_tick,Acc) -> maybe_append(Acc,<<$`>>);
-token_to_string(bang,Acc) -> maybe_append(Acc,<<$!>>);
-token_to_string(back_slash,Acc) -> maybe_append(Acc,<<$\\>>);
-token_to_string(slash,Acc) -> maybe_append(Acc,<<$/>>);
-token_to_string(hash,Acc)-> maybe_append(Acc,<<$#>>);
-token_to_string({parentheses,open},Acc) -> maybe_append(Acc,<< $(>>);
-token_to_string({parentheses,close},Acc) -> maybe_append(Acc,<<$)>>);
-token_to_string({bracket,open},Acc) -> maybe_append(Acc,<<$[>>);
-token_to_string({bracket,close},Acc) -> maybe_append(Acc,<<$]>>);
-token_to_string({{ws,sp},_},Acc) -> maybe_append(Acc,<<$\s>>);
-token_to_string({{ws,tab},_},Acc) -> maybe_append(Acc,<<$\t>>);
-token_to_string({{ws,comb},SP},Acc) ->
-    SPbin = ai_string:to_string(SP),
-    maybe_append(Acc,<<SPbin/binary>>);
-token_to_string({lf,crlf},Acc) -> maybe_append(Acc,<<"\r\n">>);
-token_to_string({lf,lf},Acc) -> maybe_append(Acc,<<"\n">>);
-token_to_string({hash,N},Acc) ->
-    Hash = lists:duplicate(N,$#),
-    HashBin = ai_string:to_string(Hash),
-    maybe_append(Acc,HashBin);
-token_to_string({binary,Bin},Acc) -> maybe_append(Acc,Bin);
-token_to_string(Other,Acc) ->[Other|Acc].
-
-
-maybe_append([{binary,Bin}|T],Other)-> [{binary,<<Bin/binary,Other/binary>>}|T];
-maybe_append(T,Other) ->[{binary,Other}|T].
-
-strip_line([],Acc)->{lists:reverse(Acc),[]};
-strip_line([{lf,_}|T],Acc)->{lists:reverse(Acc),T};
-strip_line([H|T],Acc) -> strip_line(T,[H|Acc]).
-
-
-accumulate_until_empty(Line) -> accumulate_until_empty(Line,<<>>,line).
-
-accumulate_until_empty(<<"\n",Line/binary>>,Acc,line)->
-    accumulate_until_empty(Line,<<Acc/binary,"\n">>,empty);
-accumulate_until_empty(<<"\n",Line/binary>>,Acc,empty) ->
-    {<<Acc/binary,"\n">>,Line};
-accumulate_until_empty(<<C/utf8,Line/binary>>,Acc,_) ->
-    accumulate_until_empty(Line,<<Acc/binary,C/utf8>>,line).
-
-
-
-
-    
-
-remove_space(<<"\s",Line/binary>>)-> remove_space(Line);
-remove_space(<<"\t",Line/binary>>) ->remove_space(Line);
-remove_space(Rest) -> Rest.
 
 -define(BLOCK_SCANER,[
                       fun scan_newline/1,
                       fun scan_indented/1,
                       fun scan_code/1,
-                      fun scan_atx/1
+                      fun scan_blockquote/1,
+                      fun scan_atx/1,
+                      fun scan_paragraph/1
                      ]).
+
+
+
+%accumulate_line(Line) -> accumulate_line(Line,<<>>).
+%accumulate_line(<<>>,Acc)->{Acc,eof};
+%accumulate_line(<<"\n",Line/binary>>,Acc)-> {<<Acc/binary>>,Line};
+%accumulate_line(<<C/utf8,Line/binary>>,Acc) -> accumulate_line(Line,<<Acc/binary,C/utf8>>).
+
+combine_backquote(Block)->
+    combine_backquote(Block,[]).
+combine_backquote([],Acc)-> lists:reverse(Acc);
+combine_backquote([I],Acc)-> lists:reverse([I|Acc]);
+combine_backquote([#node{kind = quote} = N1,
+                   #node{kind = quote} = N2 | T],Acc)->
+    N1Depth = proplists:get_value(depth,N1#node.attrs,0),
+    N2Depth = proplists:get_value(depth,N2#node.attrs,0),
+    if
+        N1Depth >= N2Depth ->
+            N1Value = N1#node.value,
+            N2Value = N2#node.value,
+            combine_backquote([#node{
+                                  kind = quote,
+                                  attrs = N1#node.attrs,
+                                  value = <<N1Value/binary,N2Value/binary>>,
+                                  children = N1#node.children ++ N2#node.children
+                                 } | T],Acc);
+        true ->
+            Diff = N2Depth - N1Depth,
+            Children = combine_backquote(
+                         N1#node.children ++ [N2#node{attrs = [{depth,Diff}]}],
+                         []),
+            combine_backquote([N1#node{children = Children}|T], Acc)
+    end;
+combine_backquote([H|T],Acc) -> combine_backquote(T,[H|Acc]).
+
+
+
+
+
+
+
+remove_space(<<"\s",Line/binary>>)-> remove_space(Line);
+remove_space(<<"\t",Line/binary>>) ->remove_space(Line);
+remove_space(Rest) -> Rest.
+
 
 scan_block(Line) -> scan_block(?BLOCK_SCANER,Line,[]).
 scan_block(_,eof,Acc)-> lists:reverse(Acc);
@@ -219,7 +73,8 @@ scan_block([H|T],Line,Acc) ->
                 MethodSize == 0 -> {error,lists:reverse(Acc),Line};
                 true -> scan_block(T,Line,Acc)
             end;
-        {Node,Rest} -> scan_block(?BLOCK_SCANER,Rest,[Node|Acc])
+        {Node,Rest} when erlang:is_tuple(Node)-> scan_block(?BLOCK_SCANER,Rest,[Node|Acc]);
+        {Nodes,Rest} -> scan_block(?BLOCK_SCANER,Rest,Nodes ++ Acc)
 end.
 
 scan_newline(Line)-> scan_newline(remove_space(Line),#node{kind=newline}).
@@ -251,11 +106,38 @@ scan_code(<<"~~~",Line/binary>>,Buffer,Node,content)-> scan_code(remove_space(Li
 scan_code(<<"\n",Line/binary>>,Buffer,Node,space)-> {Node#node{value = Buffer},Line};
 scan_code(<<C/utf8,Line/binary>>,Buffer,Node,content) -> scan_code(Line,<<Buffer/binary,C/utf8>>,Node,content).
 
-scan_backquote(Line)-> scan_backquote(remove_space(Line),[],#node{kind = quto},start).
-scan_backquote(<<">",Line/binary>>,Buffer,Node,start)-> stop;
-scan_backquote(Line,Buffer,Node,start)-> stop;
+scan_blockquote(Line)-> scan_blockquote(Line,[]).
 
+scan_blockquote(Line,Acc)->
+    case scan_blockquote(remove_space(Line),<<>>,#node{kind = quote},start) of
+        {Node, eof} -> {combine_backquote(lists:reverse([Node|Acc])),eof};
+        {Node,Rest} -> scan_blockquote(Rest,[Node|Acc]);
+        stop ->
+            if erlang:length(Acc) > 0 ->
+                    {combine_backquote(lists:reverse(Acc)),Line};
+               true -> stop
+            end
+    end.
 
+scan_blockquote(<<">",Line/binary>>,Buffer,Node,start)-> scan_blockquote(Line,Buffer,Node,{content,start});
+scan_blockquote(_Line,_Buffer,_Node,start)-> stop;
+scan_blockquote(<<"\s",Line/binary>>,Buffer,Node, {content,start})->
+    scan_blockquote(Line,<<Buffer/binary,"\s">>,Node,{content,start});
+scan_blockquote(<<"\t",Line/binary>>,Buffer,Node, {content,start})->
+    scan_blockquote(Line,<<Buffer/binary,"\t">>,Node,{content,start});
+scan_blockquote(<<">",Line/binary>>,_Buffer, #node{ attrs = Attrs} = Node, {content,start})->
+    case proplists:get_value(depth,Attrs) of
+        undefined -> scan_blockquote(Line,<<>>,Node#node{attrs = [{depth,1}| Attrs]},{content,start});
+        N ->
+            NewAttrs = lists:filter(fun({Key,_Attr}) -> Key /= depth end,Attrs),
+            scan_blockquote(Line,<<>>,Node#node{attrs = [{depth,N + 1}| NewAttrs]},{content,start})
+    end;
+scan_blockquote(<<>>,Buffer,Node,{content,content})->
+    {Node#node{value = <<Buffer/binary>>},eof};
+scan_blockquote(<<"\n",Line/binary>>,Buffer,Node,{content,content})->
+    {Node#node{value = <<Buffer/binary,"\n">>},Line};
+scan_blockquote(<<C/utf8,Line/binary>>,Buffer,Node,{content,_}) ->
+    scan_blockquote(Line,<<Buffer/binary,C/utf8>>,Node,{content,content}).
 
 
 scan_atx(Line)-> scan_atx(remove_space(Line),<<>>,#node{kind = heading},start).
@@ -268,7 +150,7 @@ scan_atx(<<"#",Line/binary>>,Buffer,Node,hash)->
     NewBuffer = <<Buffer/binary,"#">>,
     if
         erlang:byte_size(NewBuffer) > 6 -> stop;
-        true -> scan_atx(Line,NewBuffer,Node,start)
+        true -> scan_atx(Line,NewBuffer,Node,hash)
     end;
 scan_atx(Line,Buffer,#node{ attrs = Attrs } = Node,hash) ->
     AtxSize = erlang:byte_size(Buffer),
@@ -312,3 +194,11 @@ scan_atx(<<C/utf8,Line/binary>>,Buffer,#node{value = Acc } = Node,{content, T} =
         _ ->
             scan_atx(Line,Buffer,Node#node{ value = <<Acc/binary,C/utf8>> },S)
     end.
+
+scan_paragraph(Line)->
+    scan_paragraph(remove_space(Line),<<>>,#node{kind = paragraph}).
+
+scan_paragraph(<<>>,Buffer,Node)-> {Node#node{value = Buffer},eof};
+scan_paragraph(<<"\t",Line/binary>>,Buffer,Node)-> {Node#node{value = Buffer},Line};
+scan_paragraph(<<"\s\s\s\s",Line/binary>>,Buffer,Node) -> {Node#node{value = Buffer},Line};
+scan_paragraph(<<C/utf8,Line/binary>>,Buffer,Node) -> scan_paragraph(Line,<<Buffer/binary,C/utf8>>,Node).
