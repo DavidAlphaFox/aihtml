dbe524731291a39dbb5fb82b6ead75edcfc02d74
David.Gao <david.alpha.fox@gmail.com>
2021-12-22 09:10:18 +0800

    add MIT license

--- src/ai_mustache_parser.erl
+++ src/ai_mustache_parser.erl
@@ -1,3 +1,33 @@
+%% The MIT License (MIT)
+%%
+%% Copyright (c) 2015 Hinagiku Soranoba
+%%
+%% Permission is hereby granted, free of charge, to any person obtaining a copy
+%% of this software and associated documentation files (the "Software"), to deal
+%% in the Software without restriction, including without limitation the rights
+%% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+%% copies of the Software, and to permit persons to whom the Software is
+%% furnished to do so, subject to the following conditions:
+%%
+%% The above copyright notice and this permission notice shall be included in all
+%% copies or substantial portions of the Software.
+%%
+%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+%% SOFTWARE.
+%% @copyright 2015 Hinagiku Soranoba All Rights Reserved.
+%%
+%% @doc Binary pattern match Based Mustach template engine for Erlang/OTP.
+%%
+%% Please refer to [the man page](http://mustache.github.io/mustache.5.html) and [the spec](https://github.com/mustache/spec) of mustache as the need arises.<br />
+%%
+%% Please see [this](../benchmarks/README.md) for a list of features that bbmustache supports.
+%% 
+
 -module(ai_mustache_parser).
 
 -export([parse/1]).
